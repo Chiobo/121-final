f86ad568a741d1b7d580e72370ac1b2ae6e0fa85
Rishi Kothari <hey@rishi.cx>
2024-12-02 14:05:02 -0500

    library impl

--- LibraryUser.cpp
+++ LibraryUser.cpp
@@ -2,59 +2,60 @@
 #include "LibraryUser.hpp" // class declaration
 #include <string>
 
-// Parent Class LibraryUser 
+// Parent Class LibraryUser
 
-    // defining constructors
-    LibraryUser::LibraryUser() : name(), user_id(), borrowed_count(0) {} // default constructor
-    LibraryUser::LibraryUser(const std::string& name, const std::string& user_id): name(name), user_id(user_id) {}
+// defining constructors
+LibraryUser::LibraryUser() : name(), user_id(), borrowed_count(0) {} // default constructor
+LibraryUser::LibraryUser(const std::string &name, const std::string &user_id)
+{
+	LibraryUser();
+	this->name = name;
+	this->user_id = user_id;
+}
 
+// getter functions for private class variables
+std::string LibraryUser::get_name()
+{
+	return name;
+};
 
-    // getter functions for private class variables
-    std::string LibraryUser::get_name(){
-        return name;
-    };
+std::string LibraryUser::User_ID()
+{
+	return user_id;
+};
 
-    std::string LibraryUser::User_ID(){
-        return user_id;
-    };
+int LibraryUser::get_borrowed_count()
+{
+	return borrowed_count;
+};
 
-    int LibraryUser::get_borrowed_count(){
-        return borrowed_count;
-    };
+// functions
+void LibraryUser::borrow_book()
+{
+	borrowed_count++;
+};
 
-    // functions
-    void LibraryUser::borrow_book(){
-        borrowed_count++;
-    };
+void LibraryUser::return_book()
+{
+	if (borrowed_count == 0)
+	{
+		std::cout << "No books to return" << std::endl;
+		return;
+	}
+	borrowed_count--;
+};
 
-    void LibraryUser::return_book(){
-        borrowed_count--;
-    };
+void LibraryUser::print_user()
+{
+	std::cout << "Username: " << name << "User_ID: " << user_id << "Borrowed Count: " << borrowed_count << std::endl;
+};
+int LibraryUser::get_borrow_limit()
+{
+	std::cout << "Borrow Limit: 0" << std::endl;
+	return 0;
+}
+bool LibraryUser::check_genre(std::string genre)
+{
 
-    void LibraryUser::print_user(){
-        std::cout << "Username: " << name << "User_ID: " << user_id << "Borrowed Count: " << borrowed_count << std::endl;
-    };
-
-// Teacher Class
-
-    // defining constructors
-    Teacher::Teacher() : borrow_limit() {}
-    Teacher::Teacher(const int borrow_limit) : borrow_limit(borrow_limit) {}
-
-    // defining getter function
-    int Teacher::get_borrow_limit(){
-        return borrow_limit;
-    };
-
-    void Teacher::add_book_to_library(Library books_list, Book* book){
-        books_list.push_back(*book);
-    };
-
-    // defining print
-
-    void Teacher::print(){
-        LibraryUser::print_user();
-        std::cout << "Borrow Limit: " << borrow_limit << std::endl;
-    };
-
-
+	return true;
+};
