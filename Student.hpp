31b2a7b6343340b9b80e57abf20ab5a99b61320d
Rishi Kothari <hey@rishi.cx>
2024-12-02 14:04:48 -0500

    student class

---
+++ Student.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "LibraryUser.hpp"
+
+class Student : public LibraryUser
+{
+private:
+	bool graduated;
+	int borrow_limit;
+	std::vector<std::string> valid_genres;
+
+public:
+	Student();
+	Student(const std::string &name, const std::string &user_id, const bool graduated, const int borrow_limit, const std::vector<std::string> valid_genres);
+
+	int get_borrow_limit();
+	void print();
+	bool check_genre(std::string genre);
+};
