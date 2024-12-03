6238669116e1299ba53ae53fcb0a392c280f81c2
Rishi Kothari <hey@rishi.cx>
2024-12-02 21:36:19 -0500

    driver code

--- main.cpp
+++ main.cpp
@@ -11,16 +11,17 @@ int main()
 	// Book Class Calls
 	Book book1("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "1997", "Fantasy");
 	Library lib1;
-	lib1.insert(book1);
 	// lib1.remove(book1);
+
+	lib1.insert("Harry Potter and the Chamber of Secrets", "J.K. Rowling", "1998", "Fantasy2");
+	lib1.insert(book1);
+	lib1.insert(book1);
+
 	lib1.print();
 
-	// lib1.insert("Harry Potter and the Chamber of Secrets", "J.K. Rowling", "1998", "Fantasy2");
+	lib1.advanced_search("waefawef");
 
-	// lib1.print();
-
-	// Book *searched = lib1.advanced_search("", "", "Fantasy2");
-	// searched->print();
+		// searched->print();
 
 	// lib1.advanced_search("", "", "Fantasy")->print();
 	return 0;
