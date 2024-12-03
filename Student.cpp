38949f96914117570ad2d37a580c2106c1f9aeae
Rishi Kothari <hey@rishi.cx>
2024-12-02 22:31:59 -0500

    student impl

--- Student.cpp
+++ Student.cpp
@@ -6,11 +6,11 @@
 
 // Student Class
 Student::Student() : graduated(), borrow_limit() {}
-Student::Student(const std::string &name, const std::string &user_id, const bool graduated, const int borrow_limit, const std::vector<std::string> valid_genres)
+Student::Student(const std::string &name, const std::string &user_id, const bool graduated, const int borrow_limit, const std::vector<std::string> valid_genres) : LibraryUser(name, user_id)
 {
-	LibraryUser(name, user_id);
 	this->graduated = graduated;
 	this->borrow_limit = borrow_limit;
+	this->valid_genres = valid_genres;
 }
 
 int Student::get_borrow_limit()
@@ -20,13 +20,16 @@ int Student::get_borrow_limit()
 
 void Student::print()
 {
-	std::cout << "Student: ";
-	LibraryUser::print_user();
-	std::cout << "Graduated: " << graduated << "Borrow Limit: " << borrow_limit << std::endl;
+	std::cout << "✏️ Student: " << std::endl;
+	std::cout << indent_output("\t", [&]()
+							   { LibraryUser::print_user();
+							   
+	std::cout << "🎓 Graduated: " << (graduated ? "True" : "False") << std::endl << "⛔ Borrow Limit: " << borrow_limit << std::endl; });
 };
 
 bool Student::check_genre(std::string genre)
 {
+
 	for (int i = 0; i < valid_genres.size(); i++)
 	{
 		if (valid_genres[i] == genre)
