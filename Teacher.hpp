cb567aa834ad664decd8c148960b5790d787b8a3
Rishi Kothari <hey@rishi.cx>
2024-12-02 22:32:07 -0500

    teacher head

--- Teacher.hpp
+++ Teacher.hpp
@@ -4,6 +4,8 @@
 #include "LibraryUser.hpp"
 #include "lib.hpp"
 
+class Library;
+
 class Teacher : public LibraryUser
 {
 private:
@@ -14,10 +16,10 @@ public:
 	Teacher();
 	Teacher(const std::string &name, const std::string &user_id, const int borrow_limit, const std::vector<std::string> valid_genres);
 
-	int get_borrow_limit();
+	int get_borrow_limit() override;
 	void add_book_to_library(Library book_list, Book *book);
 	void update_book_details(Library lib, const std::string &title, const std::string &author, const std::string &year_published, const std::string &genre);
-	bool check_genre(std::string genre);
+	bool check_genre(std::string genre) override;
 
 	void print();
 };
