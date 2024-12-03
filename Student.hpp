d02f98e1662bf82002433f280ae2f29546dd9ccc
Rishi Kothari <hey@rishi.cx>
2024-12-02 22:32:05 -0500

    student header

--- Student.hpp
+++ Student.hpp
@@ -12,7 +12,7 @@ public:
 	Student();
 	Student(const std::string &name, const std::string &user_id, const bool graduated, const int borrow_limit, const std::vector<std::string> valid_genres);
 
-	int get_borrow_limit();
+	int get_borrow_limit() override;
 	void print();
-	bool check_genre(std::string genre);
+	bool check_genre(std::string genre) override;
 };
