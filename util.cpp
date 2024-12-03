a6450e3096942c1f705f2ebcc9bec3aa85f98614
Rishi Kothari <hey@rishi.cx>
2024-12-02 22:52:47 -0500

    add utils

--- util.cpp
+++ util.cpp
@@ -4,9 +4,10 @@
 #include <sstream>
 #include <string>
 
+// Utility function to indent following lines for std (useful when logging multiple objects or contextualizi8ng)
 std::string indent_output(const std::string &prefix, const std::function<void()> &print_func)
 {
-	// Capture the output
+	// Capture the output; we store std::cout in a buffer
 	std::stringstream buffer;
 	std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
 	print_func();
@@ -17,7 +18,19 @@ std::string indent_output(const std::string &prefix, const std::function<void()>
 	std::string line;
 	while (std::getline(buffer, line))
 	{
+		// Iterate over the buffer adding prefix to the start of the line
 		result << prefix << line << std::endl;
 	}
+
 	return result.str();
+}
+
+// Utility function to convert a string to lowercase
+std::string to_lower(const std::string &str)
+{
+	// Copy string
+	std::string lower_str = str;
+	// Apply the <string> tolower function to each character in the string
+	std::transform(lower_str.begin(), lower_str.end(), lower_str.begin(), ::tolower);
+	return lower_str;
 }
--- util.hpp
+++ util.hpp
@@ -4,3 +4,4 @@
 #include <string>
 
 std::string indent_output(const std::string &prefix, const std::function<void()> &print_func);
+std::string to_lower(const std::string &str);
